50ea16a6643d362bcd3dcef74c7ba4ce1ac8bb62
theicebear <dylan058@outlook.com>
2016-04-15 14:27:19 +0800

    reformat code and update docs

--- test/methods.js
+++ test/methods.js
@@ -1,13 +1,13 @@
 var request = require('supertest')
 var koa = require('koa')
 
-var router = require('..')();
+var router = require('..')()
 
 var app = koa()
 
 app.use(function* (next) {
   try {
-    router.assertImplementsMethod().apply(this);
+    router.assertImplementsMethod().apply(this)
   } catch (err) {
     this.status = 501
     return
@@ -28,46 +28,46 @@ router.search('/kasdjflkajsdf', function* (next) {
   this.status = 204
 })
 
-describe('request.assertImplementsMethod()', function(){
+describe('request.assertImplementsMethod()', function () {
   it('should throw if not implemented', function (done) {
     request(server)
-    .patch('/')
-    .expect(501, done)
+      .patch('/')
+      .expect(501, done)
   })
 
   it('should not throw if implemented', function (done) {
     request(server)
-    .get('/')
-    .expect(204, done)
+      .get('/')
+      .expect(204, done)
   })
 })
 
-describe('OPTIONS', function(){
+describe('OPTIONS', function () {
   it('should send Allow', function (done) {
     request(server)
-    .options('/')
-    .expect('Allow', /\bGET\b/)
-    .expect('Allow', /\bHEAD\b/)
-    .expect('Allow', /\bOPTIONS\b/)
-    .expect(204, done)
+      .options('/')
+      .expect('Allow', /\bGET\b/)
+      .expect('Allow', /\bHEAD\b/)
+      .expect('Allow', /\bOPTIONS\b/)
+      .expect(204, done)
   })
 })
 
-describe('405 Method Not Allowed', function(){
+describe('405 Method Not Allowed', function () {
   it('should send Allow', function (done) {
     request(server)
-    .search('/')
-    .expect('Allow', /\bGET\b/)
-    .expect('Allow', /\bHEAD\b/)
-    .expect('Allow', /\bOPTIONS\b/)
-    .expect(405, done)
+      .search('/')
+      .expect('Allow', /\bGET\b/)
+      .expect('Allow', /\bHEAD\b/)
+      .expect('Allow', /\bOPTIONS\b/)
+      .expect(405, done)
   })
 })
 
-describe('HEAD', function(){
+describe('HEAD', function () {
   it('should respond with GET if not defined', function (done) {
     request(server)
-    .head('/')
-    .expect(204, done)
+      .head('/')
+      .expect(204, done)
   })
 })
